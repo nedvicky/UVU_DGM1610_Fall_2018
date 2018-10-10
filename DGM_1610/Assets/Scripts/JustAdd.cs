9f27119e7675822706de10865ae54a78e46c04e0
Khoi Nguyen <nedvicky20@gmail.com>
2018-10-10 16:09:47 -0600

    finished level manager and added an inheritance script file

---
+++ DGM_1610/Assets/Scripts/JustAdd.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JustAdd : MonoBehaviour {
+
+    // vaiable 1 and 2 has interger
+    public int YouSuck;
+    public int YouLost;
+    
+    int TotalOfHowYouSuck;
+
+    //create function that is going to compute these two functions
+
+    void Start()
+    {
+        CalcAdd();
+    }
+
+        public void CalcAdd()
+        {
+            Add(YouSuck, YouLost);
+        }
+
+        public void Add(int yousuck, int youlost)
+        {
+            TotalOfHowYouSuck = YouLost + YouSuck;
+        }
+
+
+
+        public static void Main(int TotalOfHowYouSuck)
+        {
+            Console.WriteLine(TotalOfHowYouSuck);
+        }
+
+}
---
+++ DGM_1610/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject CurrentCheckPoint;
+    private Rigidbody2D Player;
+    //these are all declarations
+    //declaring particles
+    public GameObject DeathParticles;
+    public GameObject RespawnParticles;
+
+    //respawn delay float
+    public float RespawnDelay;
+
+    //point penalty interger for death
+    public int PointPenaltyOnDeath;
+
+    //how much gravity on respawn so you dont punch through the dang ground
+    private float GravityStore;
+
+    // Use this for initialization
+	void Start ()
+    {
+        Player = FindObjectOfType<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RespawnPlayer() //spawn delay that the player activates themselves by dying.
+    {
+        StartCoroutine("RespawnPlayerCo"); //a background cycle that is similar to an update function
+    }
+
+    public IEnumerator RespawnPlayerCo()
+    {
+        //generate death BLOOD (particles adjustment)
+        Instantiate(DeathParticles, Player.transform.transform.position, Player.transform.rotation);
+        //hide player after death
+        //Player.enabled = false;
+        Player.GetComponent<Renderer>().enabled = false;
+        //gravity reset (for when player died and is hiding)
+        GravityStore = Player.GetComponent<Rigidbody2D>().gravityScale;
+        Player.GetComponent<Rigidbody2D>().gravityScale = 0f;
+        Player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        // Point Penalty
+        ScoreManager.AddPoints(-PointPenaltyOnDeath);
+        //Debug Message
+        Debug.Log("Player Respawn");
+        //Respawn Delay
+        yield return new WaitForSeconds(RespawnDelay);
+        //Gravity Restore
+        Player.GetComponent<Rigidbody2D>().gravityScale = GravityStore;
+        //Match Players transform position
+        Player.transform.position = CurrentCheckPoint.transform.position;
+        //Show Player
+        //Player.enabled = true;
+        Player.GetComponent<Renderer>().enabled = true;
+        //Spawn Player
+        Instantiate(RespawnParticles, CurrentCheckPoint.transform.position, CurrentCheckPoint.transform.rotation);
+    }
+}
