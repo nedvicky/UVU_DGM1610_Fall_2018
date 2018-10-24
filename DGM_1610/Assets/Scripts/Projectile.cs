44d75252d8985a6440b7581654998bf61f8e2b7c
Khoi Nguyen <nedvicky20@gmail.com>
2018-10-24 15:41:19 -0600

    added projectile to player and still did not fix villain

---
+++ DGM_1610/Assets/Scripts/Projectile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float Speed;
+    public Rigidbody2D Protagonist;
+
+    public GameObject EnemyDeath;
+
+    public GameObject ProjectileParticle;
+
+    public int PointsForKill;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (Protagonist.transform.localScale.x < 0)
+            Speed = -Speed;
+
+        // Speed = Speed * Mathf.Sign(Protagonist.transform.localScale.x);
+
+        // GetComponent<Rigidbody2D>().velocity = new Vector2(Speed + (Protagonist.GetComponent<Rigidbody2D>()
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Speed, GetComponent<Rigidbody2D>().velocity.y);
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Enemy")
+        {
+            Instantiate(EnemyDeath, other.transform.position, other.transform.rotation);
+            Destroy (other.gameObject);
+            ScoreManager.AddPoints (PointsForKill);
+        }
+
+        Instantiate(ProjectileParticle, transform.position, transform.rotation);
+        Destroy (gameObject);
+    }
+
+}
