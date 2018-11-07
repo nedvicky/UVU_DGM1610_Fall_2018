d7f74e7bc65732368d199947ffa7c7fb3b0e683c
Khoi Nguyen <nedvicky20@gmail.com>
2018-11-07 15:39:25 -0700

    changed how the cam follows the player

--- DGM_1610/Assets/Scripts/CameraFollow.cs
+++ DGM_1610/Assets/Scripts/CameraFollow.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
+    public CharacterMove Protagonist;
+
+    public bool isFollowing;
+
+
+    //camera position offset
+    public float xOffset;
+    public float yOffset;
+
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
+        Protagonist = FindObjectOfType<CharacterMove>();
+
+        isFollowing = true;
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        if (isFollowing)
+        {
+            transform.position = new Vector3(Protagonist.transform.position.x + xOffset, Protagonist.transform.position.y + yOffset, transform.position.y);
+        }
 		
 	}
 }
